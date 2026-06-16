fcaccd286afc5e727d3ed86c3a012f8a48ee4fd5
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-06-16 20:20:15 -0300

    Base da API e esteira do CI

---
+++ Validador.java
@@ -0,0 +1,15 @@
+public class Validador {
+    public static void main(String[] args) {
+        System.out.println("Iniciando verificação...")
+
+        boolean bancoPronto = true;
+
+        if (bancoPronto) {
+            System.out.println("SISTEMA OK!!! SUCESSO ");
+            System.exit(0);
+        }else{
+            System.out.println("SISTEMA DEU RUIM!!!!");
+            System.exit(1);
+        }
+    }
+}
